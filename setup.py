46e84cf7c1521ee4c7dc65b97a71af022286b3b5
jiangao1203 <54560204+jiangao1203@users.noreply.github.com>
2021-02-18 03:52:35 -0600

    Third commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My final project for WUSTL course 577',
+    author='JianGao',
+    license='MIT',
+)
